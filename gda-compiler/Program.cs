362be3920a58929b881175cad5994f2e6c75b02b
Don Duvall <don@deduvall.com>
2017-01-21 16:15:09 -0800

    Lexes Decimal, Binary, Octal & Hex now

--- gda-compiler/Program.cs
+++ gda-compiler/Program.cs
@@ -38,11 +38,36 @@ namespace gda_compiler
             //  Int can transition into Float
             CharDFA.State sta_int = cdfa.NewState("int");
             CharDFA.State sta_int_end = cdfa.NewState("end_int", "INT_B10");
+
+            CharDFA.State sta_int_zero = cdfa.NewState("int_zero");
+            CharDFA.State sta_int_bin = cdfa.NewState("int_bin");
+            CharDFA.State sta_int_bin_end = cdfa.NewState("int_bin_end", "INT_B2");
+            CharDFA.State sta_int_oct = cdfa.NewState("int_oct");
+            CharDFA.State sta_int_oct_end = cdfa.NewState("int_oct_end", "INT_B8");
+            CharDFA.State sta_int_hex = cdfa.NewState("int_hex");
+            CharDFA.State sta_int_hex_end = cdfa.NewState("int_hex_end", "INT_B16");
+
             sta_start.NewDelta("start_to_int", "[1-9]", sta_int);
-            sta_start.NewDelta("start_to_zero_int", "[0]", sta_int_end);
+            sta_start.NewDelta("start_to_int_zero", "[0]", sta_int_zero);
             sta_subtract.NewDelta("subtract_to_int", "[0-9]", sta_int);
+
             sta_int.NewDelta("int_to_int", "[0-9]", sta_int);
             sta_int.NewDelta("int_to_end", "[^0-9\\.]", sta_int_end, false, false);
+
+            sta_int_zero.NewDelta("int_zero_to_int_bin", "[b]", sta_int_bin);
+            sta_int_zero.NewDelta("int_zero_to_int_oct", "[o]", sta_int_oct);
+            sta_int_zero.NewDelta("int_zero_to_int_hex", "[x]", sta_int_hex);
+            sta_int_zero.NewDelta("int_zero_to_int_end", "[^box]", sta_int_end, false, false);
+
+            sta_int_bin.NewDelta("int_bin_to_int_bin", "[01]", sta_int_bin);
+            sta_int_bin.NewDelta("int_bin_to_int_bin_end", "[^01]", sta_int_bin_end);
+
+            sta_int_oct.NewDelta("int_oct_to_int_oct", "[0-7]", sta_int_oct);
+            sta_int_oct.NewDelta("int_oct_to_int_oct_end", "[^0-7]", sta_int_oct_end);
+
+            sta_int_hex.NewDelta("int_hex_to_int_hex", "[0-9A-F]", sta_int_hex);
+            sta_int_hex.NewDelta("int_hex_to_int_hex_end", "[^0-9A-F]", sta_int_hex_end);
+
 
             // Parse Float
             // Floats are only started from an int, once a decimal point is found
@@ -88,21 +113,46 @@ namespace gda_compiler
             CharDFA.State sta_semicolon = cdfa.NewState("semicolon", "SEMICOLON");
             sta_start.NewDelta("start_to_semicolon", "[;]", sta_semicolon);
 
-            // Parse Assign
+            // Parse Assign & equality
             CharDFA.State sta_assign = cdfa.NewState("assign", "ASSIGN");
+            CharDFA.State sta_assign_end = cdfa.NewState("assign_end", "ASSIGN");
             sta_start.NewDelta("start_to_assign", "[=]", sta_assign);
+            sta_assign.NewDelta("start_to_assign", "[^=]", sta_assign_end, false, false);
+            CharDFA.State sta_equals = cdfa.NewState("equals", "EQ");
+            sta_assign.NewDelta("start_to_assign", "[=]", sta_equals);
 
             // Parse Equality
             CharDFA.State sta_lt = cdfa.NewState("lt", "LT");
+            CharDFA.State sta_lt_end = cdfa.NewState("lt_end", "LT");
             sta_start.NewDelta("start_to_lt", "[<]", sta_lt);
+            CharDFA.State sta_lteq = cdfa.NewState("lteq", "LTEQ");
+            sta_lt.NewDelta("lt_to_lteq", "[=]", sta_lteq);
+            sta_lt.NewDelta("start_to_lt", "[^=]", sta_lt_end, false, false);
+
+            CharDFA.State sta_gt = cdfa.NewState("gt", "GT");
+            CharDFA.State sta_gt_end = cdfa.NewState("gt_end", "GT");
+            sta_start.NewDelta("start_to_gt", "[>]", sta_gt);
+            CharDFA.State sta_gteq = cdfa.NewState("gteq", "GTEQ");
+            sta_gt.NewDelta("gt_to_gteq", "[=]", sta_gteq);
+            sta_gt.NewDelta("start_to_gt", "[^=]", sta_gt_end, false, false);
+
+
 
             List<CharDFA.Token> tokens = cdfa.ProcessFile("filename Memory", @"
                 
                 int main() {
                     int rg:i = 0;
+                    int st:binInt = 0b1100;
+                    int st:octInt = 0o137;
+                    int st:hexInt = 0xAFFB10;
                     for ( ; rg:i < 30; rg:i++) {
                         printf(rg:i);
                     }
+
+                    if (rg:i == 31) {
+                        return 1;
+                    }
+                    return 0;
                 }
             ");
 
